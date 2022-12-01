a79df59d68c5296bccdb15b06aa60c30a1c14a0e
B96R7 <bennetrohwer.123@gmail.com>
2022-12-01 16:08:25 +0100

    morecode

---
+++ Basic Mathematical Operations  .js
@@ -0,0 +1,16 @@
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+function basicOp(operation, value1, value2) {
+  if (operation === "+") {
+    return value1 + value2;
+  } else if (operation === "-") {
+    return value1 - value2;
+  } else if (operation === "*") {
+    return value1 * value2;
+  } else {
+    return value1 / value2;
+  }
+}
