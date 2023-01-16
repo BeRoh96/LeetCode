9fcabec969c1541831a801da12e055da974d667f
B96R7 <bennetrohwer.123@gmail.com>
2023-01-16 09:20:24 +0100

    morecode

---
+++ jan2023/splits.js
@@ -0,0 +1,17 @@
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+function solution(str) {
+  let i = 0;
+  let result = [];
+
+  if (str.length % 2 !== 0) {
+    str = str + "_";
+  }
+
+  while (i < str.length) {
+    result.push(str[i] + str[i + 1]);
+    i += 2;
+  }
+
+  return result;
+}
