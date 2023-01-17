25f2d22f4fad00ea0e564f82ef56d7806fe0b1bd
B96R7 <bennetrohwer.123@gmail.com>
2023-01-17 09:46:13 +0100

    morecode

---
+++ jan2023/consecutiveLetters.js
@@ -0,0 +1,10 @@
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+function findMissingLetter(array) {
+  let first = array[0].charCodeAt(0);
+
+  for (let i = 1; i < array.length; i++) {
+    if (first + i !== array[i].charCodeAt(0)) {
+      return String.fromCharCode(first + i);
+    }
+  }
+}
