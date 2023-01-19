2c01e4ba6e00549e8dd1348d40914863f4c22617
B96R7 <bennetrohwer.123@gmail.com>
2023-01-19 09:44:13 +0100

    moreCode

---
+++ jan2023/BuildTower.js
@@ -0,0 +1,13 @@
+// Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
+function towerBuilder(nFloors) {
+  let tower = [];
+
+  for (let i = 0; i < nFloors; i++) {
+    tower.push(
+      " ".repeat(nFloors - i - 1) +
+        "*".repeat(i * 2 + 1) +
+        " ".repeat(nFloors - i - 1)
+    );
+  }
+  return tower;
+}
