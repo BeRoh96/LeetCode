c19e649e9d5146f389803c7f1d0323725dfa9c2a
B96R7 <bennetrohwer.123@gmail.com>
2023-01-21 09:51:38 +0100

    morecode

---
+++ jan2023/leepYear.js
@@ -0,0 +1,12 @@
+// In this kata you should simply determine, whether a given year is a leap year or not. In case you don't know the rules, here they are:
+function isLeapYear(year) {
+  if (year % 400 === 0) {
+    return true;
+  } else if (year % 100 === 0) {
+    return false;
+  } else if (year % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
