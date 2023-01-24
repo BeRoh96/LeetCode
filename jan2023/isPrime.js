daa2fd8a8e621e9df24c4f033511ee1c4c519613
B96R7 <bennetrohwer.123@gmail.com>
2023-01-24 09:45:03 +0100

    morecode

---
+++ jan2023/isPrime.js
@@ -0,0 +1,11 @@
+// Define a function that takes an integer argument and returns a logical value true or false depending on if the integer is a prime.
+function isPrime(num) {
+  if (num < 2) return false;
+  const limit = Math.sqrt(num);
+  for (let i = 2; i <= limit; ++i) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
