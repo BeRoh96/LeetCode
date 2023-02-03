1969faaf0b32d7364a096dd09b7d987ddf8fc445
B96R7 <bennetrohwer.123@gmail.com>
2023-02-03 09:36:01 +0100

    morecode

---
+++ feb2023/Pattern.js
@@ -0,0 +1,9 @@
+// You have to write a function pattern which returns the following Pattern(See Pattern & Examples) upto n number of rows.
+function pattern(n) {
+  var res = [],
+    i;
+  for (i = 1; i <= n; i++) {
+    res.push(Array(i + 1).join(i));
+  }
+  return res.join("\n");
+}
