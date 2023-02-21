20e72ddca51db6f36f41808c53eaec64f7ec2f7e
B96R7 <bennetrohwer.123@gmail.com>
2023-02-21 10:34:43 +0100

    morecode

---
+++ feb2023/Date.js
@@ -0,0 +1,4 @@
+// You're re-designing a blog and the blog's posts have the following format for showing the date and time a post was made:
+function shortenToDate(longDate) {
+  return longDate.split(",")[0];
+}
